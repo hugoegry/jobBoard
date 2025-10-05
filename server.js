4ea6906ff88f72d426685ca0514c2bc17c501b76
hugoegry <hugo.egry@epitech.eu>
2025-10-05 11:34:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
